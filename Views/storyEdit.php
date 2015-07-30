2914308a01adc40ba033cc06f4e57ad1aa89b3f7
Alejandro Gardea <AlexRogerG@gmail.com>
2015-07-30 09:22:33 -0600

    Include Statements Added
    Minor change, added include statements to the dashboard, login,
    register, and storyedit files.

--- Views/storyEdit.php
+++ Views/storyEdit.php
@@ -5,3 +5,10 @@
  * Date: 7/28/2015
  * Time: 9:38 AM
  */
+    include DIR_TMPL . '/header.php';
+    include DIR_TMPL . '/navbar.php'; ?>
+
+    <h3>Edit Story</h3>
+
+
+<?php include DIR_TMPL . '/footer.php'; ?>
