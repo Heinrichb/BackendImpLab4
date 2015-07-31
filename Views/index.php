2896a78243ab08a3a8d3adf1487823d5c23f1d29
Kelsey StClaire <kstclaire95@gmail.com>
2015-07-31 09:57:12 -0600

    index css

--- Views/index.php
+++ Views/index.php
@@ -4,6 +4,45 @@
 
 <div class="container">
 
+      <div class="container-fluid" id="cont">
+            <div class="jumbotron">
+                  <h2>Catchy slogan</h2>
+                  <p>info to draw you into the site</p>
+            </div>
+
+
+            <div class="row">
+                  <div class="col-xs-6 col-md-4">
+                        <h3>Sign Up</h3>
+                        <p>words words words</p>
+                        <p>
+                           <a class="btn btn-default" href="/register" role="button">Sign Up</a>
+                        </p>
+                  </div>
+                  <div class="col-xs-6 col-md-4">
+                        <h3>About</h3>
+                        <p>words words words</p>
+                        <p>
+                              <a class="btn btn-default" href="#" role="button">Read More</a>
+                        </p>
+                  </div>
+                  <div class="col-xs-6 col-md-4">
+                        <h3>Something Else</h3>
+                        <p>words words words</p>
+                        <p>
+                              <a class="btn btn-default" href="#" role="button">Read More</a>
+                        </p>
+                  </div>
+            </div>
+
+            <div class="panel panel-default">
+                  <div class="panel-heading">Panel heading</div>
+                  <div class="panel-body">
+                        <p>sample story blah blah </p>
+                  </div>
+            </div>
+
+      </div>
 </div
 
 
