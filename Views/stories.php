16b4094ca95c99abfacdc7a2c311a6819e59bc94
nkagee <nkagee@student.neumont.edu>
2015-08-03 09:58:30 -0600

    Got rid of weird print out thing

--- Views/stories.php
+++ Views/stories.php
@@ -4,13 +4,6 @@ include DIR_TMPL.'/navbar.php'; ?>
 <section class="container">
     <h1 class="text-center">Story List</h1>
     <?php
-    foreach ($cursor as $post) {
-        echo "<p>" . $post["title"] . "<br/>" . $post["content"] . '<br/>';
-
-//        print_r($post["additions"]);
-
-        echo '</p>';
-    }
         foreach ($cursor as $post) {
             echo '<a href="/story/' . $post["_id"] . '"><div class="col-sm-6 storyContainer">';
                 echo '<h2>' . $post["title"] . '</h2>';
